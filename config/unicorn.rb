d92d2c071781a2bfadfac2d72cafc120804e4cdb
Ivan Chasov <chasow@yandex.ru>
2015-08-02 12:50:08 +0300

    изменил настройки unicorn

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,23 +1,42 @@
-worker_processes 2
-working_directory "/home/chasov/www/moreturov/"
+# Set the current app's path for later reference. Rails.root isn't available at
+# this point, so we have to point up a directory.
+app_path = File.expand_path(File.dirname(__FILE__) + '/..')
 
+# The number of worker processes you have here should equal the number of CPU
+# cores your server has.
+worker_processes (ENV['RAILS_ENV'] == 'production' ? 4 : 1)
+
+# You can listen on a port or a socket. Listening on a socket is good in a
+# production environment, but listening on a port can be useful for local
+# debugging purposes.
+listen app_path + '/tmp/sockets/unicorn.sock', backlog: 64
+
+# For development, you may want to listen on port 3000 so that you can make sure
+# your unicorn.rb file is soundly configured.
+listen(3000, backlog: 64) if ENV['RAILS_ENV'] == 'development'
+
+timeout 300
+
+working_directory app_path
+
+stderr_path app_path + "/log/unicorn.stderr.log"
+stdout_path app_path + "/log/unicorn.stdout.log"
+
+# Load the app up before forking.
 preload_app true
 
-timeout 30
+# Garbage collection settings.
+GC.respond_to?(:copy_on_write_friendly=) &&
+  GC.copy_on_write_friendly = true
 
-listen "/home/chasov/www/moreturov/tmp/sockets/unicorn.sock", :backlog => 64
-
-pid "/home/chasov/www/moreturov/tmp/pids/unicorn.pid"
-
-stderr_path "/home/chasov/www/moreturov/log/unicorn.stderr.log"
-stdout_path "/home/chasov/www/moreturov/log/unicorn.stdout.log"
-
+# If using ActiveRecord, disconnect (from the database) before forking.
 before_fork do |server, worker|
-    defined?(ActiveRecord::Base) and
-        ActiveRecord::Base.connection.disconnect!
+  defined?(ActiveRecord::Base) &&
+    ActiveRecord::Base.connection.disconnect!
 end
 
+# After forking, restore your ActiveRecord connection.
 after_fork do |server, worker|
-    defined?(ActiveRecord::Base) and
-        ActiveRecord::Base.establish_connection
+  defined?(ActiveRecord::Base) &&
+    ActiveRecord::Base.establish_connection
 end
